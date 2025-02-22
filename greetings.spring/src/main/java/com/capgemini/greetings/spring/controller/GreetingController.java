6f5f5944ec28a31e872f3ac9a36d6db94f186269
himanshu3232 <himanshu2000ranjan@gmail.com>
2025-02-22 12:33:55 +0530

    init

---
+++ greetings.spring/src/main/java/com/capgemini/greetings/spring/controller/GreetingController.java
@@ -0,0 +1,21 @@
+package com.capgemini.greetings.spring.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public ResponseEntity<String> getGreeting(@RequestParam(value="name", defaultValue = "World") String name){
+        return new ResponseEntity<>(String.format("Hello, %s", name), HttpStatus.OK);
+    }
+
+    @PostMapping("/greeting/{name}")
+    public ResponseEntity<String> postGreeting(@PathVariable("name") String name){
+        return new ResponseEntity<>(String.format("Hello, %s", name), HttpStatus.OK);
+    }
+
+    @
+}
