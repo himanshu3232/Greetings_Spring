788d6bcd0683d2cb702ede5e3401cdc4f3832d9d
himanshu3232 <himanshu2000ranjan@gmail.com>
2025-02-24 01:47:16 +0530

    [Himanshu Ranjan] Add. uc3 greeting with default hello world

--- greetings.spring/src/main/java/com/capgemini/greetings/spring/controller/GreetingController.java
+++ greetings.spring/src/main/java/com/capgemini/greetings/spring/controller/GreetingController.java
@@ -28,7 +28,12 @@ public class GreetingController extends ServiceLayer {
         return new ResponseEntity<>(String.format("Delete, %s", name), HttpStatus.OK);
     }
 
-    @GetMapping()
+    @GetMapping("greeting")
+    public ResponseEntity<String> greetings
+            (@RequestParam(value = "firstName", defaultValue = "Hello") String firstName
+                    , @RequestParam(value = "lastName", defaultValue = "World") String lastName){
+        return new ResponseEntity<>(firstName + " " + lastName, HttpStatus.OK);
+    }
 }
 
 
